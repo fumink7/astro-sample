4ea078cc9861b817eb18ef99c2b652877cac9915
fumink7 <711fumi@pepabo.com>
2023-11-27 19:47:53 +0900

    Try vanilla-extract

--- src/components/AppChild/AppChild.css.ts
+++ src/components/AppChild/AppChild.css.ts
@@ -3,4 +3,16 @@ import { style } from "@vanilla-extract/css";
 export const textStyle = style({
   color: "blue",
   fontSize: "20px",
+  ":hover": {
+    color: "red",
+  },
+});
+
+export const textStyle2 = style({
+  color: "blue",
+  fontSize: "20px",
+  padding: 20,
+  ":hover": {
+    color: "red",
+  },
 });
--- src/components/AppChild/index.tsx
+++ src/components/AppChild/index.tsx
@@ -1,4 +1,9 @@
 import * as style from "./AppChild.css.ts";
 export function AppChild() {
-  return <div className={style.textStyle}>Hello from AppChild!</div>;
+  return (
+    <>
+      <div className={style.textStyle}>Hello from AppChild!</div>
+      <div className={style.textStyle2}>Hello from AppChild2!</div>
+    </>
+  );
 }
